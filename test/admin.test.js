425fbc799f44b7c3ec7ebd08c7ed3833014ed5f5
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-14 00:40:52 +0100

    add test for admin to update a book

--- test/admin.test.js
+++ test/admin.test.js
@@ -4,7 +4,11 @@ let Admin = require('../app_methods/admin');
 describe('test cases for Admin methods', function () {
   let moses = new Admin('Moses Adebayo', 'mosesm@gmail.com');
   moses.save();
-  let grace = new Admin('Grace Igbokwe', 'graceg@gmail.com')
+  let grace = new Admin('Grace Igbokwe', 'graceg@gmail.com');
+  grace.save();
+  moses.createBook('Physics1', 'David Mogbeyi', 5);
+  grace.createBook('Agriculture1', 'Mike Ogbonna', 10);
+
   describe('test for creating an admin', function () {
     test('checks for instance of Admin', function () {
       expect(moses instanceof Admin).toBeTruthy();
@@ -12,11 +16,16 @@ describe('test cases for Admin methods', function () {
     });
   });
   describe('test for admin creating a book', function () {
-    moses.createBook('Physics1', 'David Mogbeyi', 5);
-    grace.createBook('Agriculture1', 'Mike Ogbonna', 10);
-    test('checks that new book was created', function () {
+    test('confirms that new book was created', function () {
       expect(database.books[0]).toHaveProperty('name', 'Physics1');
       expect(database.books[1]).toHaveProperty('name', 'Agriculture1');
     });
+  })
+  describe('test for admin updating a book', function () {
+    test('confirms that book was updated', function () {
+      moses.updateBook(1, {totalQuantity: 8});
+      console.log(database.books[0]);
+      expect(database.books[0]).toHaveProperty('totalQuantity', 8); 
+    })
   })
 })
--- test/user.test.js
+++ test/user.test.js
@@ -5,8 +5,6 @@ let Admin = require('../app_methods/admin')
 describe('test for user methods', function () {
   let daniel = new User('Daniel Ephraim', 'danield@gmail.com');
   daniel.save();
-  let cynthia = new User('Cynthia Ibrahim', 'cythiac@gmail.com')
-  cynthia.save();
   let moses = new Admin('Moses Adebayo', 'mosesm@gmail.com');
   moses.save();
 
@@ -14,13 +12,12 @@ describe('test for user methods', function () {
     test('check for instance of User', function () {
       expect(daniel instanceof User).toBeTruthy();
       expect(database.users[0]).toHaveProperty('name', 'Daniel Ephraim');
-      expect(database.users.length).toBe(3);
+      expect(database.users.length).toBe(2);
     });
   });
   describe('test for user searching a book by name', function () {
     moses.createBook('Physics1', 'David Mogbeyi', 5);
     moses.createBook('Agriculture1', 'Mike Ogbonna', 10);
-    console.log(daniel.searchBook('Physics1'));
     test('returns an object of the searched book', function () {
       expect(daniel.searchBook('Physics1')[0]).toHaveProperty('author', 'David Mogbeyi');
     })
