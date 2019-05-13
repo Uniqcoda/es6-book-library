ad5315fca02283423dca9b5164efb608f6a8407e
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-13 20:30:27 +0100

    add method for creating instance of a user and pass test

--- test/user.test.js
+++ test/user.test.js
@@ -4,7 +4,9 @@ let User = require('../app_methods/user')
 describe('test cases for creating a user',
   function () {
     let daniel = new User('Daniel Ephraim', 'danield@gmail.com');
+    daniel.save();
     let cynthia = new User('Cynthia Ibrahim', 'cythiac@gmail.com')
+    cynthia.save()
     test('check for instance of User', function () {
       expect(daniel instanceof User).toBeTruthy();
       expect(cynthia instanceof User).toBeTruthy();
