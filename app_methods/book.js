c8b2197239976e5173eb6d2bcd0ef2a3e96a6bed
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-13 21:31:46 +0100

    add test case for creating a book

---
+++ app_methods/book.js
@@ -0,0 +1,7 @@
+let database = require('../app_methods/database');
+
+function Book() {
+  
+}
+
+module.exports = Book;
---
+++ test/book.test.js
@@ -0,0 +1,12 @@
+let database = require('../app_methods/database');
+let Book = require('../app_methods/book');
+
+describe('test cases for books', function () {
+  let agriculture1 = new Book('Agriculture1', 'Mike Ogbonna', '10');
+  test('checks for instance of Book', function () {
+    expect(agriculture1 instanceof Book).toBeTruthy();
+    expect(database.books[0]).toHaveProperty('name', 'Agriculture1')
+  })
+  console.log(agriculture1);
+  
+})
