77fb49b2016429af0d90f7ea8b3ea654e51dddd4
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-13 21:43:55 +0100

    add method for creating a book an pass test

--- test/book.test.js
+++ test/book.test.js
@@ -5,8 +5,6 @@ describe('test cases for books', function () {
   let agriculture1 = new Book('Agriculture1', 'Mike Ogbonna', '10');
   test('checks for instance of Book', function () {
     expect(agriculture1 instanceof Book).toBeTruthy();
-    expect(database.books[0]).toHaveProperty('name', 'Agriculture1')
-  })
-  console.log(agriculture1);
-  
+    expect(database.books[0]).toHaveProperty('name', 'Agriculture1');
+  });
 })
