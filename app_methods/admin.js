619e9fd819f3a9dc71802c20dd6016b373699e03
Mary Etokwudo <etokwudomary@gmail.com>
2019-05-13 20:45:32 +0100

    add method for creating instance of an admin and pass test

--- app_methods/admin.js
+++ app_methods/admin.js
@@ -2,8 +2,10 @@ let database = require('../app_methods/database');
 let User = require('../app_methods/user');
 
 // Admin constructor
-function Admin() {
-  
+function Admin(name, email) {
+  User.call(this, name, email);
 }
 
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.constructor = Admin;
 module.exports = Admin;
